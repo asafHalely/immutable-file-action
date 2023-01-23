5ded21db207f28d1984684f205a3e6a6d6dd5791
asaf halely <asafhalely@gmail.com>
2023-01-23 12:01:48 +0200

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import git
 from actions_toolkit.github import Context
 
 context = Context()
@@ -20,19 +21,32 @@ context = Context()
 
 #     print("Title is Ok")
 
-# def isPR():
-#     pull_request = context.payload.get("pull_request")
-#     if pull_request is None or pull_request.get("title") is None :
-#         print("This action should only be run with Pull Request Events")
-#         exit(1)
+# Check that this runs only in PR
+def isPR():
+    pull_request = context.payload.get("pull_request")
+    if pull_request is None or pull_request.get("title") is None :
+        print("This action should only be run with Pull Request Events")
+        exit(1)
 
+def write_branch_diff(current_branch, target_branch):
+    # Open the repository
+    repo = git.Repo(".")
+    
+    # Get the diff between the two branches
+    diff = repo.git.diff(current_branch, target_branch)
+
+    # Write the diff to a file
+    print(diff)
 
 if __name__ == "__main__":
-    print(vars(context))
-    print(context.payload.get("pull_request").get("head"))
-    print(context.payload.get("pull_request").get("head").get("ref"))
-    print(context.payload.get("pull_request").get("base"))
-    print(context.payload.get("pull_request").get("base").get("ref"))
+    # Check that this runs only in PR
+    isPR()
+
+    current_branch = context.payload.get("pull_request").get("head").get("ref")
+    target_branch = context.payload.get("pull_request").get("base").get("ref")
+    
+    write_branch_diff(current_branch, target_branch)
+
     # pull_request = context.payload.get("pull_request")
     # if pull_request is None or pull_request.get("title") is None :
     #     print("This action should only be run with Pull Request Events")
